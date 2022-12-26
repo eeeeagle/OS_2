55b7c5d817d02b9a8f53a87b5ceb9947e36042d6
Daniil Osokin <daniil.osockin@gmail.com>
2022-12-27 00:24:09 +0400

    Many changes Deleted union structs Added const paths to files Added params for Sum and Sqr functions

--- Source.hpp
+++ Source.hpp
@@ -1,18 +1,40 @@
-#include <Windows.h>
-#include <iostream>
-
 #ifndef SOURCE
 #define SOURCE
 
-union double_convert
+#include <Windows.h>
+#include <iostream>
+
+constexpr auto VALUE_NUM = 3;
+
+static const wchar_t* values_path  = L"\\\\.\\FileMapping\\Values";
+static const wchar_t* sum_a_path   = L"\\\\.\\FileMapping\\SumA";
+static const wchar_t* sum_b_path   = L"\\\\.\\FileMapping\\SumB";
+static const wchar_t* div_a_path   = L"\\\\.\\FileMapping\\DivA";
+static const wchar_t* div_b_path   = L"\\\\.\\FileMapping\\DivA";
+static const wchar_t* sub_path     = L"\\\\.\\FileMapping\\Sub";
+static const wchar_t* sqr_path     = L"\\\\.\\FileMapping\\Sqr";
+
+static CRITICAL_SECTION cs_sum;
+static CRITICAL_SECTION cs_div;
+static CRITICAL_SECTION cs_sub;
+static CRITICAL_SECTION cs_sqr;
+
+
+struct SumParams
 {
-    double value = 0.0;
-    byte bytes[sizeof(double)];
+    const std::wstring in, out;
 };
+
+struct DivParams
+{
+    const std::wstring in, out;
+    int divider;
+};
+
 
 DWORD WINAPI Sum(LPVOID param);
 DWORD WINAPI Div(LPVOID param);
 DWORD WINAPI Sub(LPVOID param);
-DWORD WINAPI Sqrt(LPVOID param);
+DWORD WINAPI Sqr(LPVOID param);
 
 #endif
